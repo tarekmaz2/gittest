652bae063cf00643a5b3455ed81a7ab04687a2c3
Tarek Mazkatli <tarek.mazkatli@gmail.com>
2025-02-15 10:22:37 +0100

    US-123: Beschreibung des Commits

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text; // Bitte nicht löschen!
+
+namespace git.test;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Behebung des Problems der Eingabe von z.B. Umlauten - Bitte nicht löschen!
+        Console.InputEncoding = Encoding.Unicode;
+
+        // Und ab hier kommt Ihr eigener Quellcode...
+        Console.WriteLine("Hallo, Wildau!");
+        System.Console.WriteLine("erster test");
+    }
+}
